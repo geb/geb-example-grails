f8927f19c2da33bee8a8f50a748d4fdd1371a9c5
Luke Daley <ld@ldaley.com>
2011-06-27 23:41:50 +1000

    Added links to drivers.

--- test/functional/GebConfig.groovy
+++ test/functional/GebConfig.groovy
@@ -9,6 +9,7 @@ import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.chrome.ChromeDriver
 
 // Use htmlunit as the default
+// See: http://code.google.com/p/selenium/wiki/HtmlUnitDriver
 driver = { 
 	def driver = new HtmlUnitDriver()
 	driver.javascriptEnabled = true
@@ -18,11 +19,13 @@ driver = {
 environments {
 	
 	// run as “grails -Dgeb.env=chrome test-app”
+	// See: http://code.google.com/p/selenium/wiki/ChromeDriver
 	chrome {
 		driver = { new ChromeDriver() }
 	}
 	
 	// run as “grails -Dgeb.env=firefox test-app”
+	// See: http://code.google.com/p/selenium/wiki/FirefoxDriver
 	firefox {
 		driver = { new FirefoxDriver() }
 	}
