1fc63d18d3a003120064eb278b319b2d1071dd8a
Tomas Lin <tomaslin@gmail.com>
2013-01-06 23:14:20 +0000

    automatic download of chrome driver

--- test/functional/GebConfig.groovy
+++ test/functional/GebConfig.groovy
@@ -4,30 +4,43 @@
 	See: http://www.gebish.org/manual/current/configuration.html
 */
 
-import org.openqa.selenium.htmlunit.HtmlUnitDriver
-import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.htmlunit.HtmlUnitDriver
 
 // Use htmlunit as the default
 // See: http://code.google.com/p/selenium/wiki/HtmlUnitDriver
-driver = { 
-	def driver = new HtmlUnitDriver()
-	driver.javascriptEnabled = true
-	driver
+driver = {
+    def driver = new HtmlUnitDriver()
+    driver.javascriptEnabled = true
+    driver
+}
+
+private void downloadDriver(File file, String path) {
+    if (!file.exists()) {
+        def ant = new AntBuilder()
+        ant.get(src: path, dest: 'driver.zip')
+        ant.unzip(src: 'driver.zip', dest: file.parent)
+        ant.delete(file: 'driver.zip')
+        ant.chmod(file: file, perm: '700')
+    }
 }
 
 environments {
-	
-	// run as “grails -Dgeb.env=chrome test-app”
-	// See: http://code.google.com/p/selenium/wiki/ChromeDriver
-	chrome {
-		driver = { new ChromeDriver() }
-	}
-	
-	// run as “grails -Dgeb.env=firefox test-app”
-	// See: http://code.google.com/p/selenium/wiki/FirefoxDriver
-	firefox {
-		driver = { new FirefoxDriver() }
-	}
+
+    // run as “grails -Dgeb.env=chrome test-app”
+    // See: http://code.google.com/p/selenium/wiki/ChromeDriver
+    chrome {
+        def chromeDriver = new File('test/drivers/chrome/chromedriver')
+        downloadDriver(chromeDriver, "http://chromedriver.googlecode.com/files/chromedriver_mac_23.0.1240.0.zip")
+        System.setProperty('webdriver.chrome.driver', chromeDriver.absolutePath)
+        driver = { new ChromeDriver() }
+    }
+
+    // run as “grails -Dgeb.env=firefox test-app”
+    // See: http://code.google.com/p/selenium/wiki/FirefoxDriver
+    firefox {
+        driver = { new FirefoxDriver() }
+    }
 
 }
