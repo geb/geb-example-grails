f280b2a6bfdc60fc3e98991a9200c2f9ea27fcbf
Marcin Erdmann <erdi84@gmail.com>
2016-02-22 21:36:12 +0000

    Update to the latest version of Geb

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -31,7 +31,7 @@ grails.project.dependency.resolution = {
     checksums true // Whether to verify checksums on resolve
     legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
 
-    def gebVersion = "0.13.0"
+    def gebVersion = "0.13.1"
     def seleniumVersion = "2.51.0"
     
     repositories {
